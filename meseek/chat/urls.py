6d117137668706a0b6ac417e352716e63e2f909d
Sh4M <hieu2d@gmail.com>
2018-05-15 23:13:45 +0200

    outlook API proto added

--- meseek/chat/urls.py
+++ meseek/chat/urls.py
@@ -8,4 +8,7 @@ app_name = 'chat'
 urlpatterns = [
     # Index ex: /chat/
     path('', views.index, name='index'),
+    path('home/', views.home, name='home'),
+    path('gettoken/', views.gettoken, name='gettoken'),
+    path('mail/', views.mail, name='mail')
 ]
