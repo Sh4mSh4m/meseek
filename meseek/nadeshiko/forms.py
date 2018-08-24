b30f17f651149a99fc749aa50b7527e759eff158
hieu@mb12 <hieu2d@gmail.com>
2018-08-25 01:32:48 +0200

    added missing forms file

---
+++ meseek/nadeshiko/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from .models import LessonScan
+
+class QuizzConfigurationForm(forms.Form):
+    SIZE=[(10,'Standard'),
+         (20,'Medium')]
+    Difficulté = forms.ChoiceField(choices=SIZE, widget=forms.RadioSelect(attrs={'style': 'display: inline'}))
+
+class LessonScanForm(forms.ModelForm):
+    class Meta:
+        model = LessonScan
+        fields = ('description', 'image', )
+
+
+
+
+class OCRTextForm(forms.Form):
+    VOCTYPE=[('vocabulaire','Vocabulaire'),
+         ('other', 'Other')]
+    Type = forms.ChoiceField(choices=VOCTYPE, widget=forms.RadioSelect())
+    Level = forms.IntegerField(initial=3, required=True)
+    
+    def __init__(self, *args, **kwargs):
+        wordList = kwargs.pop('wordList')
+        super(OCRTextForm, self).__init__(*args, **kwargs)
+        if wordList:
+            for word in wordList:
+                self.fields['Mot{}'.format(wordList.index(word))]=forms.CharField(initial=word, required=False)
